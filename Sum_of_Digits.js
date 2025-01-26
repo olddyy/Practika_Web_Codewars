07641fc6b82bbfa4268246483592b94656527371
olddyy <158603693+olddyy@users.noreply.github.com>
2025-01-26 21:24:58 +0200

    finish two task

---
+++ Sum_of_Digits.js
@@ -0,0 +1,23 @@
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// function digitalRoot(n) {
+// let array = String(n).split("").map(Number);
+// n=0;
+
+
+// for(let i=0; i<array.length; i++){
+//     n+= array[i];
+// }
+// if (n >= 10){
+//     return digitalRoot(n);
+//     } 
+// return n;
+// }
+
+
+
+
+// console.log(digitalRoot(493193))
